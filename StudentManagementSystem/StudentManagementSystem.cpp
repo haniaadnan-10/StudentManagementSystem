9aa257526217229ebad1b61136ef057ea809938a
haniaadnan-10 <hania.adnan2000@gmail.com>
2025-04-27 17:08:35 +0500

    Final change

--- StudentManagementSystem/StudentManagementSystem.cpp
+++ StudentManagementSystem/StudentManagementSystem.cpp
@@ -1,20 +1,114 @@
-// StudentManagementSystem.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void displayMenu() {
+	cout << "Welcome to Student Management System." << endl;
+	cout << "1. Add Student." << endl;
+	cout << "2. View Student." << endl;
+	cout << "3. Uptade Student." << endl;
+	cout << "4. Delete Student." << endl;
+	cout << "5. Exit." << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void addStudent(vector<string>& students) {
+	string name;
+	cout << "Enter the name of student: " << "\n";
+	cin.ignore();
+	getline(cin, name);
+	students.push_back(name);
+	cout << "Student Added Successfully." << "\n";
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void viewStudent(const vector<string>& students) {
+	if (students.empty()) {
+		cout << "No student to show." << "\n";
+	}
+	else {
+		for (int i = 0; i < students.size(); i++) {
+			cout << i << ":" << students[i] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void uptadeStudent(vector<string>& students) {
+	int index;
+	string newName;
+	cout << "Enter the index of the student name you want to uptade: ";
+	cin >> index;
+
+	if (index >= 0 && index <= students.size()) {
+		cout << "Enter the new Name: " << endl;
+		cin >> newName;
+		students[index] = newName;
+		cout << "Student Name uptaded succesfully." << endl;
+	}
+	else {
+		cout << "Invalid index number.";
+	}
+
+}
+
+void deleteStudent(vector<string>& students) {
+	int index;
+	cout << "Enter the index of the student you want to delete: ";
+	cin >> index;
+
+	if (index >= 0 && index < students.size()) {
+		students.erase(students.begin() + index);
+		cout << "Student deleted succesfully." << endl;
+	}
+	else {
+		cout << "Invalid index." << endl;
+	}
+}
+
+int main() {
+	int choice;
+	char ans;
+	vector<string> students;
+	do {
+		displayMenu();
+		cout << "Enter your choice: ";
+		cin >> choice;
+
+		switch (choice) {
+		case 1:
+			do {
+				addStudent(students);
+				cout << "Do you want to add another student?" << "\n";
+				cin >> ans;
+				cin.ignore();
+			} while (ans == 'Y' || ans == 'y');
+			break;
+		case 2:
+			viewStudent(students);
+			break;
+		case 3:
+			do {
+				uptadeStudent(students);
+				cout << "Do you want to uptade another student name?" << "\n";
+				cin >> ans;
+				cin.ignore();
+			} while (ans == 'Y' || ans == 'y');
+			break;
+		case 4:
+			do {
+				deleteStudent(students);
+				cout << "Do you want to delete another student?" << "\n";
+				cin >> ans;
+				cin.ignore();
+			} while (ans == 'Y' || ans == 'y');
+			break;
+		case 5:
+			cout << "Exiting the program.";
+			break;
+		default:
+			cout << "Invalid choice number.";
+		}
+	} while (choice != 5);
+
+	return 0;
+}
